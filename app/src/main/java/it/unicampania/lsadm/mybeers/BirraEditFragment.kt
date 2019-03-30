1eba155b1c64e68e85d4049de204a9d83a4e9842
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-03-30 15:53:54 +0100

    Impostata azione menu e validazione campi

--- app/src/main/java/it/unicampania/lsadm/mybeers/BirraEditFragment.kt
+++ app/src/main/java/it/unicampania/lsadm/mybeers/BirraEditFragment.kt
@@ -1,9 +1,13 @@
 package it.unicampania.lsadm.mybeers
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_birra_edit.*
 
 
 /**
@@ -27,6 +31,42 @@ class BirraEditFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.edit_birra, menu)
+    }
+
+    /**
+     * Esegue le azioni relativie alla voci del menu
+     */
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        nascondiTastiera()
+
+        when (item?.itemId) {
+
+            R.id.menuConferma -> {      // Conferma
+                if (campiValidi())
+                    ;   // Passa al salvataggio
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Controlla che tutti campi siano validi
+     */
+    private fun campiValidi(): Boolean {
+        val validi = editNome.text.length > 0 && editProduttore.text.length > 0 && editGradazione.text.length > 0
+        if (!validi)
+            Toast.makeText(activity, R.string.valCampiObbligatori, Toast.LENGTH_SHORT).show()
+        return validi
+    }
+
+    /**
+     * Nasconde la tastiera
+     */
+    private fun nascondiTastiera() {
+        activity?.currentFocus?.let {
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 
 }
