526d78cdf0343df4a37dc0abdc8a6b45c498b0fa
Pasquale Cantiello <pasquale.cantiello@unicampania.it>
2019-03-30 14:59:07 +0100

    Creato menu per azione "done" nel fragment di editing

--- app/src/main/java/it/unicampania/lsadm/mybeers/BirraEditFragment.kt
+++ app/src/main/java/it/unicampania/lsadm/mybeers/BirraEditFragment.kt
@@ -3,9 +3,7 @@ package it.unicampania.lsadm.mybeers
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 
 
 /**
@@ -19,8 +17,16 @@ class BirraEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_birra_edit, container, false)
     }
 
+    /**
+     * Passa alla creazione del menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.edit_birra, menu)
+    }
 
 }
